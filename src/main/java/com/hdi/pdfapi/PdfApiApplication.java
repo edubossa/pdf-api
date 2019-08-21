2065b160a44787301743cd413779c9ef342c1053
Eduardo Wallace da Silva <eduardo.wallace@hdi.com.br>
2019-08-21 11:04:17 -0300

    Initial commit

---
+++ src/main/java/com/hdi/pdfapi/PdfApiApplication.java
@@ -0,0 +1,13 @@
+package com.hdi.pdfapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfApiApplication.class, args);
+	}
+
+}
